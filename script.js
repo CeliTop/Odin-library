e23dd4e34c34dd60842eb0e2a70f93b5eb7fd0d0
Celian Debethune <celian.debethune@gmail.com>
2022-02-17 21:21:51 +0100

    js progression

--- script.js
+++ script.js
@@ -3,13 +3,41 @@ const form = document.querySelector("form");
 const addButton = document.querySelector(".add");
 const backButton = document.querySelector(".back");
 const confirmButton = document.querySelector(".confirm");
-
 const main = document.querySelector("main");
+
+function Book(name, author, pages) {
+  this.name = name;
+  this.author = author;
+  this.nbOfPages = pages;
+}
+
+let books = [];
 
 function handleSubmit(e) {
   e.preventDefault();
-  console.log(e);
-  console.log("odfgjlkj");
+  form.classList.remove("open");
+  const formDatas = new FormData(form);
+  const name = formDatas.get("name");
+  const author = formDatas.get("author");
+  const nbOfPages = formDatas.get("nbOfPages");
+
+  const book = new Book(name, author, nbOfPages);
+  books.push(book);
+  displayBooks(books);
+}
+
+function displayBooks(books) {
+  main.innerHTML = "";
+  divsCode = "";
+  books.forEach((book) => {
+    divsCode += `<div class="tile">
+      <h4>${book.name}</h4>
+      <p class="author">Author: ${book.author}</p>
+      <p class="nop">Number of pages: ${book.nbOfPages}</p>
+      <button class="toggle">Unread</button>
+    </div>`;
+  });
+  main.innerHTML = divsCode;
 }
 
 //Display
