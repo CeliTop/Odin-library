ddbcd82271b1aa0f208c705ce2b7b941c321cbbe
Celian Debethune <celian.debethune@gmail.com>
2022-02-17 23:15:26 +0100

    main js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const JsonData = JSON.parse(localStorage.getItem("books")) || [];
+const books = JsonData.map((element) => {
+  return new Book(
+    element.name,
+    element.author,
+    element.nbOfPages,
+    element.read,
+    element.index
+  );
+});
+
 const readSlide = document.querySelector(".slide");
 const form = document.querySelector("form");
 const addButton = document.querySelector(".add");
@@ -5,13 +16,17 @@ const backButton = document.querySelector(".back");
 const confirmButton = document.querySelector(".confirm");
 const main = document.querySelector("main");
 
-function Book(name, author, pages) {
+function Book(name, author, pages, read, index) {
   this.name = name;
   this.author = author;
   this.nbOfPages = pages;
+  this.read = read;
+  this.index = index;
 }
 
-let books = [];
+Book.prototype.changeReadStatus = function () {
+  this.read = !this.read;
+};
 
 function handleSubmit(e) {
   e.preventDefault();
@@ -20,33 +35,60 @@ function handleSubmit(e) {
   const name = formDatas.get("name");
   const author = formDatas.get("author");
   const nbOfPages = formDatas.get("nbOfPages");
+  const read = readSlide.classList.contains("read");
+  const index = books.length;
 
-  const book = new Book(name, author, nbOfPages);
+  const book = new Book(name, author, nbOfPages, read, index);
   books.push(book);
+  localStorage.setItem("books", JSON.stringify(books));
   displayBooks(books);
 }
 
 function displayBooks(books) {
-  main.innerHTML = "";
   divsCode = "";
   books.forEach((book) => {
-    divsCode += `<div class="tile">
+    divsCode += `
+      <div class="tile" data-index="${book.index}">
       <h4>${book.name}</h4>
       <p class="author">Author: ${book.author}</p>
       <p class="nop">Number of pages: ${book.nbOfPages}</p>
-      <button class="toggle">Unread</button>
+     ${
+       book.read
+         ? "<button class='toggle read'>Read</button>"
+         : "<button class='toggle'>Unread</button>"
+     }
+      <button class="delete">
+        <img class="to-delete" src="./cross-sign-svgrepo-com.svg" alt="cross" />
+      </button>
     </div>`;
   });
   main.innerHTML = divsCode;
 }
 
-//Display
-readSlide.addEventListener("click", (e) => {
-  if (readSlide.classList.contains("read")) {
-    readSlide.classList.remove("read");
+function switchToggle(target) {
+  changeReadClass(target);
+  setTimeout(
+    () =>
+      (target.innerText = target.classList.contains("read")
+        ? "Read"
+        : "Unread"),
+    50
+  );
+  books[target.parentElement.dataset.index].changeReadStatus();
+  localStorage.setItem("books", JSON.stringify(books));
+}
+
+function changeReadClass(target) {
+  if (target.classList.contains("read")) {
+    target.classList.remove("read");
   } else {
-    readSlide.classList.add("read");
+    target.classList.add("read");
   }
+}
+
+//Display
+readSlide.addEventListener("click", () => {
+  changeReadClass(readSlide);
 });
 
 addButton.addEventListener("click", (e) => {
@@ -59,3 +101,19 @@ backButton.addEventListener("click", (e) => {
 
 //Actions
 form.addEventListener("submit", handleSubmit);
+document.addEventListener("click", (e) => {
+  if (e.target && e.target.classList.contains("toggle")) {
+    switchToggle(e.target);
+  } else if (e.target && e.target.classList.contains("to-delete")) {
+    let index = e.target.parentElement.parentElement.dataset.index;
+    books.splice(index, 1);
+    //We store the books to the right place in the array
+    for (let i = index; i < books.length; i++) {
+      books[i].index--;
+    }
+    displayBooks(books);
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+});
+
+displayBooks(books);
